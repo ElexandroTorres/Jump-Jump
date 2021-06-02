52e2c14a51256057966143c4855219b5a5c78771
Elexandro Torres <elexandro.torres@gmail.com>
2021-06-01 22:58:31 -0300

    random obstacles

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefab;
     private Vector3 spawnPosition = new Vector3(25, 0, 0);
     private float startTime = 2.0f;
     private float delayTime = 1.5f;
+    private float randomDelayTime;
     private PlayerController playerController;
 
     void Start()
     {
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
-        InvokeRepeating("SpawnObstacles", startTime, delayTime);
+        randomDelayTime = Random.Range(1.5f, 2.0f);
+        InvokeRepeating("SpawnObstacles", startTime, randomDelayTime);
     }
 
     private void SpawnObstacles()
     {
+        int index = Random.Range(0, obstaclePrefab.Length);
+
         if(!playerController.gameOver)
         {
-            Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+            Instantiate(obstaclePrefab[index], spawnPosition, obstaclePrefab[index].transform.rotation);
         }
     }
 }
